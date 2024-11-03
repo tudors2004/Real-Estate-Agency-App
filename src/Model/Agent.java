5441ec0da3dc4080a3da1cc3a2ea0fbb931a8620
mihai271828 <pruneanumihai@gmail.com>
2024-11-03 15:40:16 +0200

    Added Agent and Person class

--- src/Model/Agent.java
+++ src/Model/Agent.java
@@ -1,4 +1,31 @@
 package Model;
 
-public class Agent {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agent extends Person implements HasID {
+    private Integer License_Number;
+    private List<Property> assigned_Properties;
+
+    public Agent(Integer id, Integer License_Number,String Name,  String Email, Integer Phone_Number) {
+        super(id, Name,Phone_Number,Email);
+        this.License_Number = License_Number;
+        this.assigned_Properties = new ArrayList<>();
+    }
+
+    public Integer getLicense_Numb() {
+        return License_Number;
+    }
+
+    public void setLicense_Numb(Integer License_Number) {
+        this.License_Number = License_Number;
+    }
+
+    public List<Property> getAssignedProperty() {
+        return this.assigned_Properties;
+    }
+
+    public void setAssignedProperty(List<Property> assigned_Properties) {
+        this.assigned_Properties = assigned_Properties;
+    }
 }
--- src/Model/Person.java
+++ src/Model/Person.java
@@ -1,4 +1,39 @@
 package Model;
 
-public class Person {
+
+public abstract class Person {
+    protected Integer id;
+    protected String Name;
+    protected Integer Phone_Number;
+    protected String Email;
+    public Person(Integer id, String Name, Integer phone_number, String email){
+        this.id=id;
+        this.Name=Name;
+        this.Phone_Number=phone_number;
+        this.Email=email;
+    }
+    public Integer getId(){
+        return this.id;
+    }
+    public void setID(Integer id){
+        this.id=id;
+    }
+    public String getName(){
+        return this.Name;
+    }
+    public void setName(String name){
+        this.Name=name;
+    }
+    public Integer getPhone_numb(){
+        return this.Phone_Number;
+    }
+    public void setPhone_numb(Integer phone_numb){
+        this.Phone_Number=phone_numb;
+    }
+    public String getEmail(){
+        return this.Email;
+    }
+    public void setEmail(String email){
+        this.Email=email;
+    }
 }
