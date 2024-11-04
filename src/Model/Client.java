831da8738c3ede6ebadded50cff6f32c875f2e93
mihai271828 <pruneanumihai@gmail.com>
2024-11-04 10:32:29 +0200

    Added Client

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -1,4 +1,63 @@
 package Model;
 
-public class Client {
+public class Client extends Person implements HasID {
+    ClientType client_type;
+    Preferences preferences;
+    public Client(Integer id, String name, String email, Integer phoneNumber, ClientType clientType, Preferences preferences) {
+        super(id, name,phoneNumber,email);
+        this.client_type = clientType;
+        this.preferences = preferences;
+    }
+    public enum ClientType {
+        BUYER,
+        SELLER,
+        RENTER;
+    }
+
+
+    public static class Preferences {
+        private double budget;
+        private String location;
+
+
+        public Preferences(double budget, String location) {
+            this.budget = budget;
+            this.location = location;
+        }
+
+
+        public double getBudget() {
+            return budget;
+        }
+
+        public void setBudget(double budget) {
+            this.budget = budget;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+    }
+
+    public ClientType getClientType() {
+        return this.client_type;
+    }
+
+    public void setClientType(ClientType clientType) {
+        this.client_type = clientType;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
+    }
+
 }
+
