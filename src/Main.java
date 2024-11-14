862b4f09e3caa3759fa48070d17b5dc58acf3ba1
Tudor Sarghiuta <149654961+tudors2004@users.noreply.github.com>
2024-11-14 20:04:58 +0200

    Tentativa de FileRepo :/

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,21 @@ import Service.*;
  */
 public class Main {
     public static void main(String[] args) {
-        //TODO: Toate astea trebuie sa fie: new FileRepository<>("data/exemplu.txt");
         //TODO: Trebuie sa scapam de toate afisarile din Service si sa le mutam in UI!!!!!!
-        IRepository<Agent> agentRepository = new InMemoryRepository<>();
-        IRepository<Property> propertyRepository = new InMemoryRepository<>();
-        IRepository<Contract> contractRepository = new InMemoryRepository<>();
-        IRepository<Appointment> appointmentRepository = new InMemoryRepository<>();
-        IRepository<Client> clientRepository = new InMemoryRepository<>();
-        IRepository<Review> reviewRepository = new InMemoryRepository<>();
-        IRepository<ClientPreferences> clientPreferencesRepository = new InMemoryRepository<>();
+        IRepository<Agent> agentRepository = new FileRepository<>("data/agents.txt");
+        IRepository<Property> propertyRepository = new FileRepository<>("data/properties.txt");
+        IRepository<Contract> contractRepository = new FileRepository<>("data/contracts.txt");
+        IRepository<Appointment> appointmentRepository = new FileRepository<>("data/appointments.txt");
+        IRepository<Client> clientRepository = new FileRepository<>("data/clients.txt");
+        IRepository<Review> reviewRepository = new FileRepository<>("data/reviews.txt");
+        IRepository<ClientPreferences> clientPreferencesRepository = new FileRepository<>("data/clientPreferences.txt");
+//        IRepository<Agent> agentRepository = new InMemoryRepository<>();
+//        IRepository<Property> propertyRepository = new InMemoryRepository<>();
+//        IRepository<Contract> contractRepository = new InMemoryRepository<>();
+//        IRepository<Appointment> appointmentRepository = new InMemoryRepository<>();
+//        IRepository<Client> clientRepository = new InMemoryRepository<>();
+//        IRepository<Review> reviewRepository = new InMemoryRepository<>();
+//        IRepository<ClientPreferences> clientPreferencesRepository = new InMemoryRepository<>();
         Service service = new Service(agentRepository, propertyRepository, contractRepository, appointmentRepository, clientRepository, reviewRepository,clientPreferencesRepository);
         Controller controller = new Controller(service);
         UI ui = new UI(controller);
